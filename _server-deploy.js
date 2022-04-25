1391bac99cc0bca6d3bde4c870c6eac91374175a
Thowhid31 <thowhidantor16@gmail.com>
2022-04-26 00:33:20 +0600

    deploy commit

--- _server-deploy.js
+++ _server-deploy.js
@@ -12,4 +12,22 @@
  * For each project one time
  * -----------------
  * 1. heroku create
+ * 2. make sure you: git add . git commit and git push
+ * 3. git push heroku main
+ * 4. go to heroku dashboard > current project > setting > reveal config
+ * 5. copy paste config vars from your .env file
+ * 6. make sure you have whitelisted all ip address to access mongodb
+ * -----------------
+ * UPDATE SERVER with new changes
+ * -----------------
+ * 1. Make changes.
+ * 2. make sure you: git add . git commit and git push
+ * 3. git push heroku main
+ * -----------------
+ * Connect Server with Client and deploy heroku
+ * -----------------
+ * 1. replace localhost by heroku link
+ * 2. npm run build
+ * 3. firebase deploy
+ * 
 */
